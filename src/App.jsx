0cabb8fca532982321ae3c285f5942ee93a60a1e
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-05-03 17:27:28 -0400

    Create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Scores from "./components/Scores"
 import Game from "./components/Game"
+import Footer from "./components/Footer"
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Scores />
       <Game />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+const Footer = () => {
+
+  return(
+    <footer>
+      <a href="https://github.com/Afer1991" target="_blank">
+      <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer

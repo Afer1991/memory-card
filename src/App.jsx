e300cbde6297f65c189fa8fe1e8d6c59277b8199
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-05-03 16:20:10 -0400

    Add Scores component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Scores from "./components/Scores"
 
 function App() {
   
   return (
     <>
       <Header />
+      <Scores />
     </>
   )
 }
---
+++ src/components/Scores.jsx
@@ -0,0 +1,15 @@
+
+
+const Scores = () => {
+
+  return(
+    <section>
+      <div>
+        <p>Current Score: 0</p>
+        <p>Best Score: 6</p>
+      </div>
+    </section>
+  );
+};
+
+export default Scores
